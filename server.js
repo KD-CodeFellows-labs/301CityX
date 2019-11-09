eeb65b6ed4ec2732da28252a656c21a0446192d9
AstroKD <kdreyerinseattle@gmail.com>
2019-11-09 15:14:43 -0800

    getting location from google api

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 'use strict';
+// Global Variables
+let locations = {};
 
 //App Dependencies
 const express = require('express');
 const cors = require('cors');
+const superagent = require('superagent');
 
 //Load environment vars from .env file
 require('dotenv').config();
@@ -12,17 +15,46 @@ let PORT = process.env.PORT || 3005;
 const app = express();
 app.use( cors() );
 
+//Listen for requests
 app.listen( PORT, () => console.log(`Listening on port ${PORT}`));
 
 //Route Definitions
 app.get('/helloworld', helloWorld);
+app.get('/location',getLocation);
 app.use('*', notFoundHandler);
 app.use(errorHandler);
+
+
 
 //Handlers
 function helloWorld(req, res) {
   res.status(200).send('Hello World!');
 }
+
+function getLocation(req, res) {
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${req.query.data}&key=${process.env.GEOCODE_API_KEY}`;
+
+  console.log('[url]',[url]);
+
+  if (locations[url]) {
+    res.send(locations[url]);
+    console.log('locations',locations[url]);
+  }
+  else {
+    console.log(req.query.data);
+    superagent.get(url)
+      .then(data => {
+        const geoData = data.body;
+        const newLocation = new Location(req.query.data, geoData);
+        locations[url] = newLocation;
+        res.send(newLocation);
+      })
+      .catch(() => {
+        errorHandler('Error in route', req, res);
+      });
+  }
+}
+
 
 // errors
 function notFoundHandler(req, res) {
@@ -31,5 +63,14 @@ function notFoundHandler(req, res) {
 
 function errorHandler(error, req, res) {
   console.error(error);
+  console.error(req.query.data);
   res.status(500).send(error);
+}
+
+//Constructor Functions
+function Location(query, geoData) {
+  this.search_query = query;
+  this.formatted_query = geoData.results[0].formatted_address;
+  this.latitude = geoData.results[0].geometry.location.lat;
+  this.longitude = geoData.results[0].geometry.location.lng;
 }
