447b15c77332c91a82d5b3b5788c0c1356b7d3ba
AstroKD <kdreyerinseattle@gmail.com>
2019-11-10 18:07:08 -0800

    reading and checking db for existing searches

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 'use strict';
+// Built by adapting parts of the following
+// https://github.com/codefellows/seattle-301d53/class-10/city-explorer/
+// https://github.com/RanVaknin/newrepo08/
+// https://github.com/tuckerc/cityExplorer/
+// https://github.com/IntensiveLearning94/Lab09-SQLcont
+// https://github.com/astrokd/lab-07-back-end
+
 // Global Variables
 let locations = {};
 
@@ -35,31 +42,54 @@ function helloWorld(req, res) {
   res.status(200).send('Hello World!');
 }
 
+//************    Get Location    ************************/
 function getLocation(req, res) {
   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${req.query.data}&key=${process.env.GEOCODE_API_KEY}`;
 
-  console.log('in getLocation');
+  // Check if search results are in DB
+  readFromDB(req.query.data.toLowerCase())
+    .then( data => {
+      console.log('then',data);
+      if (data.rowCount > 0) {
+        res.send(data.rows[0]);
+      }
+      else {
+        superagent.get(url)
+          .then(data => {
+            const geoData = data.body;
+            const newLocation = new Location(req.query.data.toLowerCase(), geoData);
+            res.send(newLocation);
+            // locations[url] = newLocation;
+            // console.log(newLocation);
+            newLocation.writeToDB()
+              .then(writeLocation => console.log('writeLocation',writeLocation));
+          })
+          .catch(() => {
+            errorHandler('Error in route', req, res);
+          });
+      }
+    })
 
-  if (locations[url]) {
-    res.send(locations[url]);
-    console.log('locations from memory');
-  }
-  else {
-    console.log('line48',req.query.data);
-    superagent.get(url)
-      .then(data => {
-        const geoData = data.body;
-        const newLocation = new Location(req.query.data, geoData);
-        locations[url] = newLocation;
-        console.log(newLocation);
-        newLocation.writeToDB()
-          .then(newLocation => res.send(newLocation));
-      })
-      .catch(() => {
-        errorHandler('Error in route', req, res);
-      });
-    //write to db
-  }
+  // if (locations[url]) {
+  //   // res.send(locations[url]);
+  //   // console.log('locations from memory');
+  // }
+  // else {
+  //   // console.log('superagent get',req.query.data);
+  //   superagent.get(url)
+  //     .then(data => {
+  //       const geoData = data.body;
+  //       const newLocation = new Location(req.query.data.toLowerCase(), geoData);
+  //       locations[url] = newLocation;
+  //       // console.log(newLocation);
+  //       newLocation.writeToDB()
+  //         .then(newLocation => res.send(newLocation));
+  //     })
+  //     .catch(() => {
+  //       errorHandler('Error in route', req, res);
+  //     });
+  //   //write to db
+  // }
 }
 
 // errors
@@ -84,26 +114,26 @@ function Location(query, geoData) {
 Location.prototype.writeToDB = function() {
   const SQL = `INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4) ON CONFLICT DO NOTHING RETURNING id;`;
   const values = [this.search_query, this.formatted_address, this.latitude, this.longitude];
-  console.log('in writeToDB');
-  return client.query(SQL, values)
-    .then(result => {
-      // console.log(result);
-      this.id = result.rows[0].id;
-      return this;
-    });
+  // console.log('in writeToDB');
+  return client.query(SQL, values);
+  // .then(result => {
+  //   // console.log(result);
+  //   this.id = result.rows[0].id;
+  //   console.log('then writeToDB',this.search_query,result);
+  //   return this;
+  // });
 };
 
-Location.prototype.readFromDB = function(location) {
+// Location.prototype.readFromDB = function(location) {
+function readFromDB(location) {
   const SQL = `SELECT * FROM locations WHERE search_query=$1;`;
-  const values = [location.query];
-
-  return client.query(SQL, values)
-    .then(result => {
-      if (result.rowCount > 0) {
-        location.cacheHit(result);
-      } else {
-        location.cacheMiss();
-      }
-    })
-    .catch(console.error);
-};
+  // console.log('location.query',location.query);
+  const values = [location];
+  console.log('in readFromDB',values);
+  let db = client.query(SQL, values);
+  // console.log('db',db);
+  return db;
+  // .then(result => {
+  //   console.log('then readFromDB',result.rows[0]);
+  // })
+}
